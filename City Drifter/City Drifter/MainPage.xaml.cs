242e47d4ad3ce6f822f865e52904b5b399e37b0b
Keith <keithharryman75@gmail.com>
2020-08-27 08:47:26 -0700

    initial working commit(using timer demo)

---
+++ City Drifter/City Drifter/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+using Xamarin.Forms;
+
+namespace City_Drifter
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public int HourOffset { get; private set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            var clockRefresh = new Timer(dueTime: 0, period: 1000, callback: UpdateTimeLabel, state: null);
+        }
+
+        private void UpdateTimeLabel(object state = null)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                time.Text = DateTime.Now.AddHours(HourOffset).ToLongTimeString();
+            }
+            );
+        }
+
+        private void OnUpButton_Clicked(object sender, EventArgs e)
+        {
+            HourOffset++;
+            UpdateTimeLabel();
+        }
+
+        private void OnDownButton_Clicked(object sender, EventArgs e)
+        {
+            HourOffset--;
+            UpdateTimeLabel();
+        }
+    }
+}
